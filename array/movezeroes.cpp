c8c2fba1be59443cdb57f101f20b64f05a85e82e
Ayushi <ayushi0sharma1@gmail.com>
2026-08-13 22:28:29 +0530

    dfvvr

--- array/movezeroes.cpp
+++ array/movezeroes.cpp
@@ -19,7 +19,7 @@ vector <int> movezeroes(vector<int>&nums)
 }
 int main()
 {
-   vector<int>nums={30,0,20,5,7,7,0,8,555,00,56657,8,0,0};
+   vector<int>nums={3,0,20,5,7,7,0,8,555,00,56657,8,0,0};
    vector<int>ans=movezeroes(nums);
    for(int i=0;i<ans.size();i++)
    {
